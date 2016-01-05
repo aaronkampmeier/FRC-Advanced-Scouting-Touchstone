d854e4277a503e8eae00e0a6093d7f7526996de9
Aaron Kampmeier <aaron.kampmeier@gmail.com>
2016-01-04 18:32:25 -0600

    Initial Commit

---
+++ FRC Advanced Scouting TelemetricsTests/FRC_Advanced_Scouting_TelemetricsTests.swift
@@ -0,0 +1,36 @@
+//
+//  FRC_Advanced_Scouting_TelemetricsTests.swift
+//  FRC Advanced Scouting TelemetricsTests
+//
+//  Created by Aaron Kampmeier on 1/4/16.
+//  Copyright © 2016 Kampfire Technologies. All rights reserved.
+//
+
+import XCTest
+@testable import FRC_Advanced_Scouting_Telemetrics
+
+class FRC_Advanced_Scouting_TelemetricsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
