fcdf45e080aa86aaa0bd53c0304afd1c2100539b
Aaron Kampmeier <aaron.kampmeier@gmail.com>
2016-12-18 14:17:22 -0600

    Redesigned Data Model to fit cloud integration and to align more with FIRST's systems.

--- FRC Advanced Scouting Telemetrics/AdminConsoleController.swift
+++ FRC Advanced Scouting Telemetrics/AdminConsoleController.swift
@@ -16,27 +16,17 @@ class AdminConsoleController: UIViewController, UITableViewDataSource, UITableVi
     let dataManager = TeamDataManager()
     
     override func viewDidLoad() {
-        tableView.dataSource = self
-        tableView.delegate = self
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
-        if (indexPath as NSIndexPath).row == 0 {
-            cell?.textLabel?.text = "Matches"
-            cell?.detailTextLabel?.text = "Add, Edit, and Configure Matches"
-        } else if (indexPath as NSIndexPath).row == 1 {
-			cell?.textLabel?.text = "Regionals"
-			cell?.detailTextLabel!.text = "Add, Edit, and Configure Regionals"
-		}
 		
-        return cell!
-    }
-    
+	}
+	
+	func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		<#code#>
+	}
+	
     @IBAction func donePressed(_ sender: UIBarButtonItem) {
 		dataManager.commitChanges()
         dismiss(animated: true, completion: nil)
@@ -53,20 +43,6 @@ class AdminConsoleController: UIViewController, UITableViewDataSource, UITableVi
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         super.prepare(for: segue, sender: sender)
-        
-        if segue.identifier == "pushToConfigure" {
-            let configureVC = segue.destination as! AdminConfigureVC
-            configureVC.previousViewController = self
-            
-            //Set the setting being configured
-            switch (tableView.indexPathForSelectedRow! as NSIndexPath).row {
-            case 0:
-                configureVC.configureSetting = .matches
-            case 1:
-				configureVC.configureSetting = .regionals
-            default:
-                configureVC.configureSetting = .unknown
-            }
-        }
+		
     }
 }
